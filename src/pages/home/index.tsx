cb8ca07be4885f7d7715d20bea205b9af300af88
Mateus Pragana <mateuspragana@gmail.com>
2022-11-28 19:34:27 -0300

    feat: Criar base da página da aplicação e seu component cardYourTodo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,21 @@
+import "twin.macro";
+import { CardYourTodo } from "../../components/cardYourTodo";
+
 const Home: React.FC = () => {
-  return <h1>Home page</h1>;
+  return (
+    <div tw="min-h-screen w-full bg-gray-800">
+      <div tw="flex flex-col space-y-4 min-h-screen lg:w-[876px] w-[85%] bg-gray-700 mx-auto text-center py-8 px-4 shadow-lg rounded">
+        <div tw="mb-8">
+          <h1 tw="text-white font-bold text-4xl">Welcome to YourTodo</h1>
+          <span tw="text-white text-[small] italic">
+            Aqui você faz a gestão de suas atividades !
+          </span>
+        </div>
+        <CardYourTodo />
+        <CardYourTodo />
+      </div>
+    </div>
+  );
 };
 
 export { Home };
