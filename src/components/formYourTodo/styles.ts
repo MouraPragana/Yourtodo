48454b3408b9edb94cc2a318bb67888aaecc1a58
Mateus Moura <mateuspragana@gmail.com>
2022-11-29 13:10:40 -0300

    feat: RTA para calendario white input text type date

---
+++ src/components/formYourTodo/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  input {
+    ::-webkit-calendar-picker-indicator {
+      background: url(https://www.citypng.com/public/uploads/small/11640487316tn6humbw4ocual1q5ianqwqtvbf1tlamuunybkut1cehro20zblsntmev8swdlwp5weiquxoytp0ywsdq5gsduasru0euvdhecdd.png)
+        center/80% no-repeat;
+      color: black;
+    }
+  }
+`;
