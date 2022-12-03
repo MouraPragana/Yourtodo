9d035e48166cc32698142456497d9af52f7a3000
Mateus Pragana <mateuspragana@gmail.com>
2022-12-03 13:33:06 -0300

    feat: Tipar useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const useLocalStorage = (key: string, defaultValue: string) => {
-  const [value, setValue] = useState(() => {
+  const [value, setValue] = useState<string>(() => {
     const currentValue = JSON.parse(
       localStorage.getItem(key) || JSON.stringify(defaultValue)
     );
